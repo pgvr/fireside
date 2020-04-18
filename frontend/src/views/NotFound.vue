52ca0cc953eaade4fc1b2896af4ef6e5238fa4e9
Manuel Schurr <admin@manuelschurr.com>
2020-04-18 15:38:41 +0200

    Added AppBar to /404

--- frontend/src/views/NotFound.vue
+++ frontend/src/views/NotFound.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="NotFound">
         <v-container>
+            <AppBar />
             <v-layout>
                 <v-card height="400" color="transparent" flat>
                     <div class="display-3">Page not found.</div>
@@ -9,3 +10,11 @@
         </v-container>
     </div>
 </template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator"
+import AppBar from "../components/AppBar.vue"
+
+@Component({ components: { AppBar } })
+export default class NotFound extends Vue {}
+</script>
