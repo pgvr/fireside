8d5ce5132e930878e5c71e6372ea57b1517488ae
Manuel Schurr <admin@manuelschurr.com>
2020-04-21 19:42:49 +0200

    Added correct return setting to /setting/update

--- backend/src/database/repository/setting.repo.ts
+++ backend/src/database/repository/setting.repo.ts
@@ -16,14 +16,12 @@ export default class SettingRepo {
 
     public static async update(setting: Setting): Promise<Setting> {
         setting.updatedAt = new Date()
-        await SettingModel.updateOne({ userId: setting.userId }, { $set: { ...setting } })
-            .lean()
-            .exec()
-        return setting
+        const newSetting = await SettingModel.updateOne({ userId: setting.userId }, { $set: { ...setting } }).exec()
+        return newSetting
     }
 
     public static async delete(userId: Types.ObjectId): Promise<Setting> {
-        const setting = SettingModel.findOneAndDelete({ userId })
+        const setting = await SettingModel.findOneAndDelete({ userId })
         return setting
     }
 }
