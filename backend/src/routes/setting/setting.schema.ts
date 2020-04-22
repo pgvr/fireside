fc624b7ad7fba470ee87134fb5f874d2527ff1fa
Manuel Schurr <admin@manuelschurr.com>
2020-04-22 20:43:50 +0200

    Updated settings Schema

--- backend/src/routes/setting/setting.schema.ts
+++ backend/src/routes/setting/setting.schema.ts
@@ -3,7 +3,8 @@ import Joi from "@hapi/joi"
 export default {
     setting: Joi.object().keys({
         days: Joi.array().required().items(Joi.number().min(0).max(6)),
-        hours: Joi.array().required().items(Joi.number().min(0).max(23)),
+        startTime: Joi.string().required(),
+        endTime: Joi.string().required(),
         numPerDay: Joi.number().required().min(1),
     }),
 }
