6197152c25edf43cd85b3db6e7f734d924bc2aff
Manuel Schurr <admin@manuelschurr.com>
2020-04-23 17:29:06 +0200

    Added cards to profile, setting still broken

--- frontend/src/views/Profile.vue
+++ frontend/src/views/Profile.vue
@@ -1,165 +1,182 @@
 <template>
     <Layout>
         <AppBar />
-        <v-layout column>
-            <h1 class="display-1">Profile</h1>
+        <v-layout column align-center>
             <v-form @submit.prevent="updateUser()">
-                <v-text-field
-                    prepend-icon="mdi-phone"
-                    v-model="phone"
-                    label="Phone"
-                    type="tel"
-                    readonly
-                    required
-                    :error-messages="phoneErrors()"
-                    :loading="userStateLoading()"
-                    @input="$v.phone.$touch()"
-                    @blur="$v.phone.$touch()"
-                ></v-text-field>
-                <v-text-field
-                    prepend-icon="mdi-city"
-                    v-model="city"
-                    @input="$v.city.$touch()"
-                    @blur="$v.city.$touch()"
-                    :error-messages="cityErrors()"
-                    :loading="userStateLoading()"
-                    label="City"
-                    type="text"
-                    required
-                ></v-text-field>
-                <v-combobox
-                    v-model="interests"
-                    chips
-                    :deletable-chips="true"
-                    :delimiters="[' ', ',']"
-                    clearable
-                    @blur="$v.interests.$touch()"
-                    :error-messages="interestErrors()"
-                    :loading="userStateLoading()"
-                    label="Your favorite hobbies"
-                    multiple
-                    prepend-icon="mdi-table-tennis"
-                >
-                </v-combobox>
-                <v-text-field
-                    prepend-icon="mdi-briefcase"
-                    v-model="job"
-                    label="Occupation"
-                    @input="$v.job.$touch()"
-                    @blur="$v.job.$touch()"
-                    :error-messages="jobErrors()"
-                    :loading="userStateLoading()"
-                    type="text"
-                    required
-                ></v-text-field>
-                <v-tooltip bottom>
-                    <template v-slot:activator="{ on }">
-                        <v-select
-                            :items="languages"
-                            v-model="language"
-                            prepend-icon="mdi-translate"
-                            label="Language"
+                <v-card class="mx-auto" max-width="500">
+                    <v-card-text>
+                        <h1 class="title text--primary">Profile</h1>
+                        <v-text-field
+                            prepend-icon="mdi-phone"
+                            v-model="phone"
+                            label="Phone"
+                            type="tel"
                             readonly
                             required
-                            v-on="on"
-                        ></v-select>
-                    </template>
-                    <span>We only support English for now</span>
-                </v-tooltip>
-                <v-btn large type="submit" color="primary">Update</v-btn>
+                            :error-messages="phoneErrors()"
+                            :loading="userStateLoading()"
+                            @input="$v.phone.$touch()"
+                            @blur="$v.phone.$touch()"
+                        ></v-text-field>
+                        <v-text-field
+                            prepend-icon="mdi-city"
+                            v-model="city"
+                            @input="$v.city.$touch()"
+                            @blur="$v.city.$touch()"
+                            :error-messages="cityErrors()"
+                            :loading="userStateLoading()"
+                            label="City"
+                            type="text"
+                            required
+                        ></v-text-field>
+                        <v-combobox
+                            v-model="interests"
+                            chips
+                            :deletable-chips="true"
+                            :delimiters="[' ', ',']"
+                            clearable
+                            @blur="$v.interests.$touch()"
+                            :error-messages="interestErrors()"
+                            :loading="userStateLoading()"
+                            label="Your favorite hobbies"
+                            multiple
+                            prepend-icon="mdi-table-tennis"
+                        >
+                        </v-combobox>
+                        <v-text-field
+                            prepend-icon="mdi-briefcase"
+                            v-model="job"
+                            label="Occupation"
+                            @input="$v.job.$touch()"
+                            @blur="$v.job.$touch()"
+                            :error-messages="jobErrors()"
+                            :loading="userStateLoading()"
+                            type="text"
+                            required
+                        ></v-text-field>
+                        <v-tooltip bottom>
+                            <template v-slot:activator="{ on }">
+                                <v-select
+                                    :items="languages"
+                                    v-model="language"
+                                    prepend-icon="mdi-translate"
+                                    label="Language"
+                                    readonly
+                                    required
+                                    v-on="on"
+                                ></v-select>
+                            </template>
+                            <span>We only support English for now</span>
+                        </v-tooltip>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-btn large type="submit" color="primary">Update</v-btn>
+                    </v-card-actions>
+                </v-card>
             </v-form>
 
-            <h1 class="display-1 mt-12">Scheduled Call Settings</h1>
-            <v-row v-if="!settingExists" justify="center"
-                ><v-btn @click="settingExists = true" class="mt-4">Set up scheduled calls</v-btn></v-row
-            >
-            <v-layout column v-else>
-                <v-row justify="center">
-                    <v-btn-toggle v-model="days" dense color="success" background-color="gray" multiple>
-                        <v-btn small :value="1">Mon</v-btn>
-                        <v-btn small :value="2">Tue</v-btn>
-                        <v-btn small :value="3">Wed</v-btn>
-                        <v-btn small :value="4">Thu</v-btn>
-                        <v-btn small :value="5">Fri</v-btn>
-                        <v-btn small :value="6">Sat</v-btn>
-                        <v-btn small :value="0">Sun</v-btn>
-                    </v-btn-toggle>
-                </v-row>
-                <v-row no-gutters>
-                    <v-col cols="6" class="pr-2">
-                        <v-dialog
-                            ref="startDialog"
-                            v-model="settingStartModal"
-                            :return-value.sync="startTime"
-                            persistent
-                            width="290px"
-                        >
-                            <template v-slot:activator="{ on }">
+            <v-card class="mx-auto mt-8" max-width="500">
+                <v-card-text>
+                    <h1 class="title text--primary">Scheduled Call Settings</h1>
+                    <template v-if="settingExists">
+                        <v-row justify="center">
+                            <v-btn-toggle v-model="days" dense color="success" background-color="gray" multiple>
+                                <v-btn small :value="1">Mon</v-btn>
+                                <v-btn small :value="2">Tue</v-btn>
+                                <v-btn small :value="3">Wed</v-btn>
+                                <v-btn small :value="4">Thu</v-btn>
+                                <v-btn small :value="5">Fri</v-btn>
+                                <v-btn small :value="6">Sat</v-btn>
+                                <v-btn small :value="0">Sun</v-btn>
+                            </v-btn-toggle>
+                        </v-row>
+                        <v-row no-gutters>
+                            <v-col cols="6" class="pr-2">
+                                <v-dialog
+                                    ref="startDialog"
+                                    v-model="settingStartModal"
+                                    :return-value.sync="startTime"
+                                    persistent
+                                    width="290px"
+                                >
+                                    <template v-slot:activator="{ on }">
+                                        <v-text-field
+                                            v-model="startTime"
+                                            label="Start Time"
+                                            prepend-icon="mdi-clock-outline"
+                                            readonly
+                                            :error-messages="startTimeErrors"
+                                            v-on="on"
+                                        ></v-text-field>
+                                    </template>
+                                    <v-time-picker v-if="settingStartModal" v-model="startTime" format="24hr">
+                                        <v-spacer></v-spacer>
+                                        <v-btn text color="primary" @click="settingStartModal = false">Cancel</v-btn>
+                                        <v-btn text color="primary" @click="$refs.startDialog.save(startTime)"
+                                            >OK</v-btn
+                                        >
+                                    </v-time-picker>
+                                </v-dialog>
+                            </v-col>
+                            <v-col cols="6" class="pl-2">
+                                <v-dialog
+                                    ref="endDialog"
+                                    v-model="settingEndModal"
+                                    :return-value.sync="endTime"
+                                    persistent
+                                    width="290px"
+                                >
+                                    <template v-slot:activator="{ on }">
+                                        <v-text-field
+                                            v-model="endTime"
+                                            label="End Time"
+                                            prepend-icon="mdi-clock-outline"
+                                            readonly
+                                            :error-messages="endTimeErrors"
+                                            v-on="on"
+                                        ></v-text-field>
+                                    </template>
+                                    <v-time-picker v-if="settingEndModal" v-model="endTime" format="24hr">
+                                        <v-spacer></v-spacer>
+                                        <v-btn text color="primary" @click="settingEndModal = false">Cancel</v-btn>
+                                        <v-btn text color="primary" @click="$refs.endDialog.save(endTime)">OK</v-btn>
+                                    </v-time-picker>
+                                </v-dialog>
+                            </v-col>
+                        </v-row>
+                        <v-row>
+                            <v-col cols="2"></v-col>
+                            <v-col cols="8">
                                 <v-text-field
-                                    v-model="startTime"
-                                    label="Start Time"
-                                    prepend-icon="mdi-clock-outline"
-                                    readonly
-                                    :error-messages="startTimeErrors"
-                                    v-on="on"
-                                ></v-text-field>
-                            </template>
-                            <v-time-picker v-if="settingStartModal" v-model="startTime" format="24hr">
-                                <v-spacer></v-spacer>
-                                <v-btn text color="primary" @click="settingStartModal = false">Cancel</v-btn>
-                                <v-btn text color="primary" @click="$refs.startDialog.save(startTime)">OK</v-btn>
-                            </v-time-picker>
-                        </v-dialog>
-                    </v-col>
-                    <v-col cols="6" class="pl-2">
-                        <v-dialog
-                            ref="endDialog"
-                            v-model="settingEndModal"
-                            :return-value.sync="endTime"
-                            persistent
-                            width="290px"
+                                    v-model="numPerDay"
+                                    label="Number of calls per day"
+                                    append-outer-icon="mdi-plus"
+                                    @click:append-outer="increment"
+                                    prepend-icon="mdi-minus"
+                                    @click:prepend="decrement"
+                                >
+                                </v-text-field>
+                            </v-col>
+                            <v-col cols="2"></v-col>
+                        </v-row>
+                    </template>
+                </v-card-text>
+                <v-card-actions v-if="!settingExists">
+                    <v-col align="center">
+                        <v-btn v-if="!settingExists" @click="settingExists = true" class="mt-4"
+                            >Set up scheduled calls</v-btn
                         >
-                            <template v-slot:activator="{ on }">
-                                <v-text-field
-                                    v-model="endTime"
-                                    label="End Time"
-                                    prepend-icon="mdi-clock-outline"
-                                    readonly
-                                    :error-messages="endTimeErrors"
-                                    v-on="on"
-                                ></v-text-field>
-                            </template>
-                            <v-time-picker v-if="settingEndModal" v-model="endTime" format="24hr">
-                                <v-spacer></v-spacer>
-                                <v-btn text color="primary" @click="settingEndModal = false">Cancel</v-btn>
-                                <v-btn text color="primary" @click="$refs.endDialog.save(endTime)">OK</v-btn>
-                            </v-time-picker>
-                        </v-dialog>
-                    </v-col>
-                </v-row>
-                <v-row>
-                    <v-col cols="2"></v-col>
-                    <v-col cols="8">
-                        <v-text-field
-                            v-model="numPerDay"
-                            label="Number of calls per day"
-                            append-outer-icon="mdi-plus"
-                            @click:append-outer="increment"
-                            prepend-icon="mdi-minus"
-                            @click:prepend="decrement"
+                        <v-btn v-if="settingExists" @click="updateScheduleSetting">Set up scheduled calls</v-btn>
+
+                        <v-btn v-if="settingCreated" @click="updateScheduleSetting" class="mb-2"
+                            >Update scheduled call settings</v-btn
                         >
-                        </v-text-field>
+                        <v-btn v-if="settingCreated" @click="deleteScheduleSetting" class="mt-2"
+                            >Disable scheduled calls</v-btn
+                        >
                     </v-col>
-                    <v-col cols="2"></v-col>
-                </v-row>
-                <v-row v-if="!settingCreated" justify="center">
-                    <v-btn @click="updateScheduleSetting">Set up scheduled calls</v-btn>
-                </v-row>
-                <v-col v-else align="center">
-                    <v-btn @click="updateScheduleSetting" class="mb-2">Update scheduled call settings</v-btn>
-                    <v-btn @click="deleteScheduleSetting" class="mt-2">Disable scheduled calls</v-btn>
-                </v-col>
-            </v-layout>
+                </v-card-actions>
+            </v-card>
             <v-btn style="margin-top: 800px" @click="logout">Logout<v-icon>mdi-account</v-icon></v-btn>
         </v-layout>
         <BottomNav />
@@ -205,6 +222,10 @@ export default class Profile extends Vue {
     settingEndModal = false
     settingExists = false
     settingCreated = false
+
+    get getSettingExists() {
+        return this.settingExists
+    }
 
     userId = ""
     days: number[] = []
